eecf4a95e869c0f7ff7e1e5bea3d850ed6805e15
Jesus Antonio Mendoza Tovar <mo_si_mo@hotmail.com>
2023-10-28 23:04:08 -0600

    new deployment

--- main.js
+++ main.js
@@ -1,45 +1,24 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-// Middlewares
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname )));
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-// Routes
-app.get('/', (req, res) => {
-  // Serve the "index.html" file from the root directory
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-
-app.get('/home', (req, res) => {
-  const activePage = 'home';
-  res.render('pages/home', { activePage });
-});
-
-app.get('/books', (req, res) => {
-  const activePage = 'books';
-  res.render('pages/books', { activePage });
-});
-
-app.get('/donation', (req, res) => {
-  const activePage = 'donation';
-  res.render('pages/donation', { activePage });
-});
-
-// Server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-  console.log(`To display the page go to http://localhost:${port}`);
-});
-
-
-
-
-
+setupCounter(document.querySelector('#counter'))
